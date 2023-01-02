0a83a1316cb360899415a23c705f1a45e353039a
jacob-02 <jvsanoj23@gmail.com>
2023-01-02 12:07:02 +0530

    first commit

---
+++ launch/rviz.launch.py
@@ -0,0 +1,24 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    package_share_dir = get_package_share_directory('maze_bot')
+    urdf = os.path.join(package_share_dir, 'urdf', 'maze_bot.urdf')
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            arguments=[urdf]),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',),
+    ])
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from glob import glob
+import os
+
+package_name = 'maze_bot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jacob',
+    maintainer_email='jvsanoj23@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
