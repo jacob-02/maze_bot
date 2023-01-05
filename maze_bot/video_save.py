8d2697ea718806667edec049c9ae2032a8caa1a6
jacob-02 <jvsanoj23@gmail.com>
2023-01-05 13:48:03 +0530

    Video saving

--- maze_bot/video_save.py
+++ maze_bot/video_save.py
@@ -8,15 +8,15 @@ import os
 class Video_get(Node):
     def __init__(self):
         super().__init__('video_get', parameter_overrides=[])
-        self.bridge = CvBridge()
-        self.subscription = self.create_subscription(
+        self.subscriber = self.create_subscription(
             Image,
-            '/camera/image_raw',
+            '/upper_camera/image_raw',
             self.listener_callback,
             10)
-        self.subscription  # prevent unused variable warning
+        self.subscriber  # prevent unused variable warning
         vid_path = os.path.join(os.getcwd(), 'video.avi')
         self.out = cv2.VideoWriter(vid_path, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 30, (1280, 720))
+        self.bridge = CvBridge()
 
     def listener_callback(self, msg):
         cv2_img = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
--- setup.py
+++ setup.py
@@ -27,7 +27,8 @@ setup(
     entry_points={
         'console_scripts': [
             'driving_mazebot = maze_bot.driving_node:main',
-            'go_to_goal = maze_bot.go_to_goal:main'
+            'go_to_goal = maze_bot.go_to_goal:main',
+            'video_get = maze_bot.video_save:main'
         ],
     },
 )
